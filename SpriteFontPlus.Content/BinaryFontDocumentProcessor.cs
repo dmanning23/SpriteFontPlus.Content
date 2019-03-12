62a1c5368771d12277faed7e5cc2f95cfa3fa75d
Dan Manning <dmanning23@hotmail.com>
2019-03-12 16:33:57 -0500

    Initial commit

---
+++ SpriteFontPlus.Content/BinaryFontDocumentProcessor.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using System;
+
+namespace XmlBuddy.Content
+{
+	/// <summary>
+	/// This class will be instantiated by the XNA Framework Content Pipeline
+	/// to apply custom processing to content data, converting an object of
+	/// type TInput to TOutput. The input and output types may be the same if
+	/// the processor wishes to alter data without changing its type.
+	///
+	/// This should be part of a Content Pipeline Extension Library project.
+	///
+	/// TODO: change the ContentProcessor attribute to specify the correct
+	/// display name for this processor.
+	/// </summary>
+	[ContentProcessor(DisplayName = "Font - SpriteFontPlus")]
+	public class BinaryFontDocumentProcessor : ContentProcessor<byte[], byte[]>
+	{
+		public override byte[] Process(byte[] input, ContentProcessorContext context)
+		{
+			try
+			{
+				return input;
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("There was an error processing the ttf", ex);
+			}
+		}
+	}
+}
---
+++ SpriteFontPlus.Content/BinaryFontSourceImporter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using System;
+
+namespace XmlBuddy.Content
+{
+	/// <summary>
+	/// This class will be instantiated by the XNA Framework Content Pipeline
+	/// to import a file from disk into the specified type, TImport.
+	///
+	/// This should be part of a Content Pipeline Extension Library project.
+	///
+	/// TODO: change the ContentImporter attribute to specify the correct file
+	/// extension, display name, and default processor for this importer.
+	/// </summary>
+	[ContentImporter(".ttf", DisplayName = "SpriteFontPlus Importer", DefaultProcessor = "BinaryFontDocumentProcessor")]
+	public class XmlSourceImporter : ContentImporter<byte[]>
+	{
+		public override byte[] Import(string filename, ContentImporterContext context)
+		{
+			try
+			{
+				return System.IO.File.ReadAllBytes(filename);
+			}
+			catch (Exception ex)
+			{
+				throw new Exception("There was an error importing the thing", ex);
+			}
+		}
+	}
+}
